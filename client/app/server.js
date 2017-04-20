2705f6f7d6840204c033f34c23eee573a86e933d
WinstonHoUMass <winstonho@umass.edu>
2017-04-20 14:13:46 -0400

    missionAccomplished

--- client/app/server.js
+++ client/app/server.js
@@ -1,13 +1,19 @@
-import {readDocument, writeDocument, addDocument, deleteDocument, getCollection} from './database.js';
+import {
+    readDocument,
+    writeDocument,
+    addDocument,
+    deleteDocument,
+    getCollection
+} from './database.js';
 
 /**
  * Emulates how a REST call is *asynchronous* -- it calls your function back
  * some time in the future with data.
  */
 function emulateServerReturn(data, cb) {
-  setTimeout(() => {
-    cb(data);
-  }, 4);
+    setTimeout(() => {
+        cb(data);
+    }, 4);
 }
 
 var token = 'eyJpZCI6NH0='; // <-- Put your base64'd JSON token here
@@ -85,24 +91,24 @@ function sendXHR(verb, resource, body, cb) {
  * Resolves a feed item. Internal to the server, since it's synchronous.
  */
 function getFeedItemSync(feedItemId) {
-  var feedItem = readDocument('feedItems', feedItemId);
-  // Resolve 'like' counter.
-  feedItem.likeCounter = feedItem.likeCounter.map((id) => readDocument('users', id));
-  // Assuming a StatusUpdate. If we had other types of FeedItems in the DB, we would
-  // need to check the type and have logic for each type.
-  feedItem.contents.author = readDocument('users', feedItem.contents.author);
-  // Resolve comment author.
-  feedItem.comments.forEach((comment) => {
-    comment.author = readDocument('users', comment.author);
-  });
-  return feedItem;
+    var feedItem = readDocument('feedItems', feedItemId);
+    // Resolve 'like' counter.
+    feedItem.likeCounter = feedItem.likeCounter.map((id) => readDocument('users', id));
+    // Assuming a StatusUpdate. If we had other types of FeedItems in the DB, we would
+    // need to check the type and have logic for each type.
+    feedItem.contents.author = readDocument('users', feedItem.contents.author);
+    // Resolve comment author.
+    feedItem.comments.forEach((comment) => {
+        comment.author = readDocument('users', comment.author);
+    });
+    return feedItem;
 }
 
 /**
  * Emulates a REST call to get the feed data for a particular user.
  */
 export function getFeedData(user, cb) {
-  // We don't need to send a body, so pass in 'undefined' for the body.
+    // We don't need to send a body, so pass in 'undefined' for the body.
     sendXHR('GET', '/user/' + user + '/feed', undefined, (xhr) => {
         // Call the callback with the data.
         cb(JSON.parse(xhr.responseText));
@@ -113,30 +119,26 @@ export function getFeedData(user, cb) {
  * Adds a new status update to the database.
  */
 export function postStatusUpdate(user, location, contents, cb) {
-  sendXHR('POST', '/feeditem', {
-    userId: user,
-    location: location,
-    contents: contents
-  }, (xhr) => {
-    // Return the new status update.
-    cb(JSON.parse(xhr.responseText));
-  })
+    sendXHR('POST', '/feeditem', {
+        userId: user,
+        location: location,
+        contents: contents
+    }, (xhr) => {
+        // Return the new status update.
+        cb(JSON.parse(xhr.responseText));
+    })
 }
 
 /**
  * Adds a new comment to the database on the given feed item.
  */
 export function postComment(feedItemId, author, contents, cb) {
-  var feedItem = readDocument('feedItems', feedItemId);
-  feedItem.comments.push({
-    "author": author,
-    "contents": contents,
-    "postDate": new Date().getTime(),
-    "likeCounter": []
-  });
-  writeDocument('feedItems', feedItem);
-  // Return a resolved version of the feed item.
-  emulateServerReturn(getFeedItemSync(feedItemId), cb);
+    sendXHR('Post', '/feeditem/' + feedItemId + '/comment', {
+        userId: author,
+        contents: contents
+    }, (xhr) => {
+        cb(JSON.parse(xhr.responseText));
+    })
 }
 
 /**
@@ -144,10 +146,10 @@ export function postComment(feedItemId, author, contents, cb) {
  * Provides an updated likeCounter in the response.
  */
 export function likeFeedItem(feedItemId, userId, cb) {
-  sendXHR('PUT', '/feeditem/' + feedItemId + '/likelist/' + userId,
-      undefined, (xhr) => {
-          cb(JSON.parse(xhr.responseText));
-      });
+    sendXHR('PUT', '/feeditem/' + feedItemId + '/likelist/' + userId,
+        undefined, (xhr) => {
+            cb(JSON.parse(xhr.responseText));
+        });
 }
 
 /**
@@ -155,63 +157,56 @@ export function likeFeedItem(feedItemId, userId, cb) {
  * Provides an updated likeCounter in the response.
  */
 export function unlikeFeedItem(feedItemId, userId, cb) {
-  sendXHR('DELETE', '/feeditem/' + feedItemId + '/likelist/' + userId,
-      undefined, (xhr) => {
-          cb(JSON.parse(xhr.responseText));
-      });
+    sendXHR('DELETE', '/feeditem/' + feedItemId + '/likelist/' + userId,
+        undefined, (xhr) => {
+            cb(JSON.parse(xhr.responseText));
+        });
 }
 
 /**
  * Adds a 'like' to a comment.
  */
 export function likeComment(feedItemId, commentIdx, userId, cb) {
-  var feedItem = readDocument('feedItems', feedItemId);
-  var comment = feedItem.comments[commentIdx];
-  comment.likeCounter.push(userId);
-  writeDocument('feedItems', feedItem);
-  comment.author = readDocument('users', comment.author);
-  emulateServerReturn(comment, cb);
+    sendXHR('PUT', '/feeditem/' + feedItemId + '/comments/' + commentIdx + '/likelist/' + userId,
+        undefined, (xhr) => {
+            cb(JSON.parse(xhr.responseText));
+        })
 }
 
 /**
  * Removes a 'like' from a comment.
  */
 export function unlikeComment(feedItemId, commentIdx, userId, cb) {
-  var feedItem = readDocument('feedItems', feedItemId);
-  var comment = feedItem.comments[commentIdx];
-  var userIndex = comment.likeCounter.indexOf(userId);
-  if (userIndex !== -1) {
-    comment.likeCounter.splice(userIndex, 1);
-    writeDocument('feedItems', feedItem);
-  }
-  comment.author = readDocument('users', comment.author);
-  emulateServerReturn(comment, cb);
+    sendXHR('DELETE', '/feeditem/' + feedItemId + '/comments/' + commentIdx + '/likelist/' + userId,
+        undefined, (xhr) => {
+            cb(JSON.parse(xhr.responseText));
+        });
 }
 
 /**
  * Updates the text in a feed item (assumes a status update)
  */
 export function updateFeedItemText(feedItemId, newContent, cb) {
-  sendXHR('PUT', '/feeditem/' + feedItemId + '/content', newContent, (xhr) => {
-      cb(JSON.parse(xhr.responseText));
-  });
+    sendXHR('PUT', '/feeditem/' + feedItemId + '/content', newContent, (xhr) => {
+        cb(JSON.parse(xhr.responseText));
+    });
 }
 
 /**
  * Deletes a feed item.
  */
 export function deleteFeedItem(feedItemId, cb) {
-  sendXHR('DELETE', '/feeditem/' + feedItemId, undefined, () => {
-      cb();
-  });
+    sendXHR('DELETE', '/feeditem/' + feedItemId, undefined, () => {
+        cb();
+    });
 }
 
 /**
  * Searches for feed items with the given text.
  */
 export function searchForFeedItems(userId, queryText, cb) {
-  // userID is not needed; it's included in the JSON web token.
-  sendXHR('POST', '/search', queryText, (xhr) => {
-      cb(JSON.parse(xhr.responseText));
-  });
+    // userID is not needed; it's included in the JSON web token.
+    sendXHR('POST', '/search', queryText, (xhr) => {
+        cb(JSON.parse(xhr.responseText));
+    });
 }
